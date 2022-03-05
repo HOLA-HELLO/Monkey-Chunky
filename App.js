0df5309224a18186c87405291e67d4df5982cf21
HOLA-HELLO <85765751+HOLA-HELLO@users.noreply.github.com>
2022-03-05 14:58:06 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import {
   View,
   StyleSheet,
   TextInput,
-  TouchableOpacity,
   Image,
-  Alert
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 import { Header } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import db from './localdb';
 import PhonicSoundButton from './components/PhonicSoundButton';
 
 export default class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
       text: '',
       chunks: [],
@@ -23,54 +25,47 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Header
-          backgroundColor={'#9c8210'}
-          centerComponent={{
-            text: 'Monkey Chunky',
-            style: { color: '#fff', fontSize: 20 },
-          }}
-        />
-
-        <Image
-          style={styles.imageIcon}
-          source={{
-            uri:
-              'https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png',
-          }}
-        />
-
-        <TextInput
-          style={styles.inputBox}
-          onChangeText={text => {
-            this.setState({ text: text });
-          }}
-          value={this.state.text}
-        />
-        <TouchableOpacity
-          style={styles.goButton}
-          onPress={() => {
-            var word = this.state.text.toLowerCase().trim();
-            db[word]?(
-            this.setState({ chunks: db[word].chunks }),
-            this.setState({ phonicSounds: db[word].phones })
-            ):
-            Alert.alert("The word does not exist in our database");
-          }}>
-          <Text style={styles.buttonText}>GO</Text>
-        </TouchableOpacity>
-        <View>
-          {this.state.chunks.map((item, index) => {
-            return (
-              <PhonicSoundButton
-                wordChunk={this.state.chunks[index]}
-                soundChunk={this.state.phonicSounds[index]}
-                buttonIndex={index}
-              />
-            );
-          })}
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Header
+            backgroundColor={'#ffee6b'}
+            centerComponent={{
+              text: 'Monosilábico',
+              style: { color: '#6b6b6b', fontsize: 5 },
+            }}
+          />
+          <Image style={styles.imageIcon} source={require('./mono.png')} />
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={(text) => {
+              this.setState({ text: text });
+            }}
+            value={this.state.text}
+          />
+          <TouchableOpacity
+            style={styles.goButton}
+            onPress={() => {
+              var word = this.state.text.toLowerCase().trim();
+              db[word]
+                ? (this.setState({ chunks: db[word].chunks }),
+                  this.setState({ phonicSounds: db[word].phones }))
+                : Alert.alert("The word doesn't exists");
+            }}>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableOpacity>
+          <View>
+            {this.state.chunks.map((item, index) => {
+              return (
+                <PhonicSoundButton
+                  wordChunk={this.state.chunks[index]}
+                  soundChunk={this.state.phonicSounds[index]}
+                  buttonIndex={index}
+                />
+              );
+            })}
+          </View>
         </View>
-      </View>
+      </SafeAreaProvider>
     );
   }
 }
@@ -81,7 +76,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#b8b8b8',
   },
   inputBox: {
-    marginTop: 50,
+    marginTop: 20,
     width: '80%',
     alignSelf: 'center',
     height: 40,
@@ -89,21 +84,24 @@ const styles = StyleSheet.create({
     borderWidth: 4,
     outline: 'none',
   },
+  imageIcon: {
+    width: 150,
+    height: 150,
+    marginLeft: 95,
+  },
   goButton: {
     width: '50%',
+    alingSelf: 'center',
     height: 55,
-    alignSelf: 'center',
     padding: 10,
     margin: 10,
+    justify: 'center',
   },
   buttonText: {
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 'bold',
+    alignSelf: 'center',
+    justify: 'center',
   },
-  imageIcon: {
-    width: 150,
-    height: 150,
-    marginLeft: 95,
-  }
 });
